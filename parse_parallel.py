37b491b816088e16903a321bd23a3f2fb324744c
QihangZhang <694499657@qq.com>
2023-05-22 12:31:45 +0800

    format

--- parse_parallel.py
+++ parse_parallel.py
@@ -1,8 +1,27 @@
+# %%
 from pathlib import Path
 import global_options
 import seminlpclassify
+import pandas as pd
+import os
 
+# %%
 if __name__ == "__main__":
+
+    PATH_FILE = os.path.abspath(os.path.dirname(__file__)) + '/'
+    PATH_TEXT = PATH_FILE + 'input_data/tweets_origin.txt'
+    PATH_INDEX = PATH_FILE + 'input_data/ids_origin.txt'
+
+    with open(PATH_TEXT, 'r',encoding='utf-8') as f:
+        lines = f.read().splitlines()
+
+    max_text_len = pd.Series(lines).apply(len).max()
+    
+    if max_text_len > 1000000:
+        raise ValueError(
+            f'max number of input, {max_text_len} is bigger than limitation 1000000'
+        )
+
     """Arguments"""
     seminlpclassify.l1_auto_parser(
         endpoint=global_options.ADDRESS_CORENLP,
@@ -10,7 +29,7 @@ if __name__ == "__main__":
         nlp_threads=global_options.N_CORES,
         path_input_txt=Path(global_options.INPUT_DATA_FOLDER, "tweets_origin.txt"),
         input_index_list=seminlpclassify._file_util.file_to_list(
-            Path(global_options.INPUT_DATA_FOLDER, "id_origin.txt")
+            Path(global_options.INPUT_DATA_FOLDER, "ids_origin.txt")
         ),
         path_output_txt=Path(
             global_options.PROCESSED_DATA_FOLDER, "parsed", "documents.txt"
