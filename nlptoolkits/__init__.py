7fac6268cf32dbe2b3ee4c5bc6e210ec90537e6d
QihangZhang <694499657@qq.com>
2023-09-18 17:14:14 +0800

    Change aliases making tactic, make them to directly import.

--- nlptoolkits/__init__.py
+++ nlptoolkits/__init__.py
@@ -8,32 +8,7 @@ from . import resources
 # out source pack
 
 # --------------------------------------------------------------------------
-# l0 level functions
+# directly import naive functions, aliases
 # --------------------------------------------------------------------------
 
-"""function aliases"""
-
-
-def alias_delete_whole_dir(*args, **kwargs):
-    """delete the whole dir..."""
-    _BasicKits._BasicFuncT.delete_whole_dir(*args, **kwargs)
-
-
-def alias_file_to_list(*args, **kwargs):
-    """alias to _BasicT.file_to_list"""
-    return _BasicKits.FileT.file_to_list(*args, **kwargs)
-
-
-def alias_list_to_file(*args, **kwargs):
-    """alias to _BasicT.file_to_list"""
-    _BasicKits.FileT.list_to_file(*args, **kwargs)
-
-
-def alias_write_dict_to_csv(*args, **kwargs):
-    """alias to _BasicT.write_dict_to_csv"""
-    return _BasicKits.FileT.write_dict_to_csv(*args, **kwargs)
-
-
-def alias_base64_to_file(*args, **kwargs):
-    """alias to _BasicT.base64_to_file"""
-    return _BasicKits.FileT.base64_to_file(*args, **kwargs)
+from ._BasicKits._BasicFuncT import delete_whole_dir
